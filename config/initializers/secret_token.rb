8ebbaceaaec729b1652137ae3bdefa0778810607
Ernst Riemer <ernst.riemer@xtremelabs.com>
2013-04-27 10:29:32 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigdata::Application.config.secret_token = '50c4702be215a2851c2f283391ce1474975c5911a3abe20d7b2cdeb62db9e326b2aee131781623f1c4349c77c7806b6fa5ec08973379bef3cd416651a84541d1'
