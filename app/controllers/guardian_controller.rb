68e0b0d29f72830b66b1430582d17ecf6b35f1f2
Ernst Riemer <ernst.riemer@xtremelabs.com>
2013-04-27 12:44:52 -0400

    Semantria hooked up

--- app/controllers/guardian_controller.rb
+++ app/controllers/guardian_controller.rb
@@ -1,3 +1,27 @@
+class SessionCallbackHandler < CallbackHandler
+  def onRequest(sender, args)
+    # puts "Put in a request: #{args}"
+  end
+
+  def onResponse(sender, args)
+    # puts "Got a resposne: #{args}"
+    $data_done = true
+  end
+
+  def onError(sender, args)
+    print 'Error: ', args, "\n"
+  end
+
+  def onDocsAutoResponse(sender, args)
+    #print "DocsAutoResponse: ", args.length, args, "\n"
+  end
+
+  def onCollsAutoResponse(sender, args)
+    #print "CollsAutoResponse: ", args.length, args, "\n"
+  end
+end
+
+
 class GuardianController < ApplicationController
 
   def index
@@ -17,7 +41,38 @@ class GuardianController < ApplicationController
 
     sanitized_data = ActionView::Base.full_sanitizer.sanitize(json_data["response"]["content"]["fields"]["body"])
 
-    render json: {action: "show", article: params[:id], article_data: json_data, article_body: sanitized_data}
+    doc = {'id' => rand(10 ** 10).to_s.rjust(10, '0'), 'text' => sanitized_data}
+
+    semantria_session = Session.new(Bigdata::Application.config.semantria_key, Bigdata::Application.config.semantria_secret, 'ForJusticeApp', true)
+
+    callback = SessionCallbackHandler.new
+
+    semantria_session.setCallbackHandler(callback)
+
+    # Queues document for processing on Semantria service
+    status = semantria_session.queueDocument(doc)
+
+    $data_done = false
+
+    wait_for_semantria = false
+
+    # puts "Status: #{status}"
+    puts "Status: #{status.inspect}"
+  
+    if status == 202
+      puts "Document '#{doc['id']}' queued successfully."
+      wait_for_semantria = true
+    end
+
+    semantria_result = semantria_session.getProcessedDocuments()
+
+    begin
+      puts "Waiting..."
+      # ARG
+      sleep 0.1
+    end while (wait_for_semantria && !$data_done)
+
+    render json: {action: "show", article: params[:id], article_data: json_data, article_body: sanitized_data, semantria_data: semantria_result}
   end
 
 end
--- config/initializers/semantria_keys.rb
+++ config/initializers/semantria_keys.rb
@@ -1,6 +1,9 @@
-Bigdata::Application.config.semantria_key = '6c521695-1e3c-457b-87ba-69146c63f48c'
-Bigdata::Application.config.semantria_secret = 'd7430ea3-7412-4ac8-90c9-def48d69a47a'
+# Bigdata::Application.config.semantria_key = '6c521695-1e3c-457b-87ba-69146c63f48c'
+# Bigdata::Application.config.semantria_secret = 'd7430ea3-7412-4ac8-90c9-def48d69a47a'
+
+Bigdata::Application.config.semantria_key = '4f755fe0-0968-4b0b-8ce4-9f95b5d2b47f'
+Bigdata::Application.config.semantria_secret = '2a21a23b-ec7a-42b2-85b0-0b35dbd0dbb2'
 
 # set up semantria session
 
-$semantria_session = Session.new(Bigdata::Application.config.semantria_key, Bigdata::Application.config.semantria_secret, 'TestApp', true)
+$semantria_session = Session.new(Bigdata::Application.config.semantria_key, Bigdata::Application.config.semantria_secret, 'ForJusticeApp', true)
